d575639cd69aa74559dc1ea071846c5b2bd462c9
stayslee <513382813@qq.com>
2018-07-20 13:18:06 +0800

    初始化

---
+++ test/UIImage+Rotate.h
@@ -0,0 +1,32 @@
+//
+//  UIImage+Rotate.h
+//  UIImage+Categories
+//
+//  Created by lisong on 16/9/4.
+//  Copyright © 2016年 lisong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Rotate)
+
+/** 纠正图片的方向 */
+- (UIImage *)fixOrientation;
+
+/** 按给定的方向旋转图片 */
+- (UIImage*)rotate:(UIImageOrientation)orient;
+
+/** 垂直翻转 */
+- (UIImage *)flipVertical;
+
+/** 水平翻转 */
+- (UIImage *)flipHorizontal;
+
+/** 将图片旋转degrees角度 */
+- (UIImage *)imageRotatedByDegrees:(CGFloat)degrees;
+
+/** 将图片旋转radians弧度 */
+- (UIImage *)imageRotatedByRadians:(CGFloat)radians;
+//改变图片颜色
+- (UIImage *)imageWithColor:(UIColor *)color;
+@end
